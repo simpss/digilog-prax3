e605784b7ee430ea09c15c1b71cf0a0c8af19b92
Siim Talvik <siim@talvik.eu>
2015-10-20 02:23:05 +0300

    cleanup

--- adder.vhd
+++ adder.vhd
@@ -1,12 +1,6 @@
 ------------------------------------------------------------------------------
---  File: mux.vhd
+--  File: adder.vhd
 ------------------------------------------------------------------------------
---Multiplexer design
---Control signal is CONT_SIG
---The output is chosen from 2 signals with length 8 bits
---If control signal is '1' then input A_in is chosen 
---If '0' then input B_IN is chosen to the output OUT_SIG
-
 
 library IEEE;
 use IEEE.std_logic_1164.all;
@@ -25,44 +19,43 @@ end behave_adder;
 architecture RTL of BEHAVE_ADDER is
 	--Siin saab deklareerida abisignaale ja alamkomponente
 begin
---everything will be async from here on out(after the begin), except processes, procedures and functions
-
---DISP_ADDER process
-DISP_ADDER: process ( X, Y, C_IN, C_OUT, S ) 
---Process parameters are called "sensitivity list". Defines what variable value changes does the process listen to.
---Private variables for a process:
-variable truth :std_logic_vector( 2 downto 0 );
-  begin
-    truth := X & Y & C_IN;
-    case truth is
-       when "000" => 
-          C_OUT <= '0';
-          S <= '0';
-       when "001" => 
-          C_OUT <= '0';
-          S <= '1';
-       when "010" => 
-          C_OUT <= '0';
-          S <= '1';
-       when "011" => 
-          C_OUT <= '1';
-          S <= '0';
-       when "100" => 
-          C_OUT <= '0';
-          S <= '1';
-       when "101" => 
-          C_OUT <= '1';
-          S <= '0';
-       when "110" => 
-          C_OUT <= '1';
-          S <= '0';
-       when "111" => 
-          C_OUT <= '1';
-          S <= '1';
-       when others => 
-          C_OUT <= 'U';
-          S <= 'U';
-    end case;
-  end process DISP_ADDER;
-               
+	--everything will be async from here on out(after the begin), except processes, procedures and functions
+	
+	--DISP_ADDER process
+	DISP_ADDER: process ( X, Y, C_IN, C_OUT, S ) 
+	--Process parameters are called "sensitivity list". Defines what variable value changes does the process listen to.
+	--Private variables for a process:
+	variable truth :std_logic_vector( 2 downto 0 );
+	  begin
+	    truth := X & Y & C_IN;
+	    case truth is
+	       when "000" => 
+	          C_OUT <= '0';
+	          S <= '0';
+	       when "001" => 
+	          C_OUT <= '0';
+	          S <= '1';
+	       when "010" => 
+	          C_OUT <= '0';
+	          S <= '1';
+	       when "011" => 
+	          C_OUT <= '1';
+	          S <= '0';
+	       when "100" => 
+	          C_OUT <= '0';
+	          S <= '1';
+	       when "101" => 
+	          C_OUT <= '1';
+	          S <= '0';
+	       when "110" => 
+	          C_OUT <= '1';
+	          S <= '0';
+	       when "111" => 
+	          C_OUT <= '1';
+	          S <= '1';
+	       when others => 
+	          C_OUT <= 'U';
+	          S <= 'U';
+	    end case;
+	  end process DISP_ADDER;     
 end RTL;
--- adder_tb.vhd
+++ adder_tb.vhd
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
---  File: mux_tb.vhd
+--  File: adder_tb.vhd
 ------------------------------------------------------------------------------
---Multiplexor testbench
+--Adder testbench
 library IEEE;
 use IEEE.std_logic_1164.all;
 
@@ -10,103 +10,101 @@ entity AdderTestBench is
 end AdderTestBench;
 
 architecture Bench of AdderTestBench is
-
-  --Component declaration for MUX
-component behave_adder is --Copy of Mux Entity as a component
-  port ( X     : in std_logic;
-         Y     : in std_logic;
-         C_IN  : in std_logic;
-         C_OUT : in  std_logic;
-         S     : out std_logic;
-       );
-  end component;
-
-  --Local signal declarations
-  signal X, Y,CIn, COut, S : std_logic;  
-
-begin
-
---Component instantiation of MUX
-Mux_comp: BEHAVE_ADDER port map (X, Y, CIn, COut, S);
-
---Stimulus process
-Stimulus: process
-   begin
-      X <= '0' after 0ns, '1' after 40ns, 'U' after 80ns;
-      Y <= '0' after 0ns, '1' after 20ns, '0' after 40ns, '1' after 60ns, 'U' after 80ns;
-      CIn <= '0' after 0ns, '1' after 10ns, '0' after 20ns, '1' after 30ns, '0' after 40ns, 
-             '1' after 50ns, '0' after 60ns, '1' after 70ns, 'U' after 80ns;
-      
-   wait;  --Suspend
-end process Stimulus;
-
-Assertions: process
-   begin
-      wait for 5ns;
-      assert (COut = '0')
-         report "COut should be 0 at 5ns"
-         severity WARNING;
-      assert (S = '0')
-         report "S should be 0 at 5ns"
-         severity WARNING;
-      wait for 10ns;
-      
-      assert (COut = '0')
-         report "COut should be 0 at 15ns"
-         severity WARNING;
-      assert (S = '1')
-         report "S should be 1 at 5ns"
-         severity WARNING;
-      wait for 10ns;
-      
-      assert (COut = '0')
-         report "COut should be 0 at 25ns"
-         severity WARNING;
-      assert (S = '1')
-         report "S should be 1 at 25ns"
-         severity WARNING;
-      wait for 10ns;
-      
-      assert (COut = '1')
-         report "COut should be 1 at 35ns"
-         severity WARNING;
-      assert (S = '0')
-         report "S should be 0 at 35ns"
-         severity WARNING;
-      wait for 10ns;
-      
-      assert (COut = '0')
-         report "COut should be 0 at 45ns"
-         severity WARNING;
-      assert (S = '1')
-         report "S should be 1 at 45ns"
-         severity WARNING;
-      wait for 10ns;
-      
-      assert (COut = '1')
-         report "COut should be 1 at 55ns"
-         severity WARNING;
-      assert (S = '0')
-         report "S should be 0 at 55ns"
-         severity WARNING;
-      wait for 10ns;
-      
-      assert (COut = '1')
-         report "COut should be 1 at 65ns"
-         severity WARNING;
-      assert (S = '0')
-         report "S should be 0 at 65ns"
-         severity WARNING;
-      wait for 10ns;
-      
-      assert (COut = '1')
-         report "COut should be 1 at 75ns"
-         severity WARNING;
-      assert (S = '1')
-         report "S should be 1 at 75ns"
-         severity WARNING;
-      
-      wait;  --Suspend
-end process Assertions;
-
+	--Component declaration for MUX
+	component behave_adder is --Copy of Mux Entity as a component
+	  port ( X     : in std_logic;
+	         Y     : in std_logic;
+	         C_IN  : in std_logic;
+	         C_OUT : in  std_logic;
+	         S     : out std_logic;
+	       );
+	end component;
+	
+	--Local signal declarations
+	signal X, Y,CIn, COut, S : std_logic;  
+	
+	begin
+	
+	--Component instantiation of MUX
+	Mux_comp: BEHAVE_ADDER port map (X, Y, CIn, COut, S);
+	
+	--Stimulus process
+	Stimulus: process
+	   begin
+	      X <= '0' after 0ns, '1' after 40ns, 'U' after 80ns;
+	      Y <= '0' after 0ns, '1' after 20ns, '0' after 40ns, '1' after 60ns, 'U' after 80ns;
+	      CIn <= '0' after 0ns, '1' after 10ns, '0' after 20ns, '1' after 30ns, '0' after 40ns, 
+	             '1' after 50ns, '0' after 60ns, '1' after 70ns, 'U' after 80ns;
+	      
+	   wait;  --Suspend
+	end process Stimulus;
+	
+	Assertions: process
+	   begin
+	      wait for 5ns;
+	      assert (COut = '0')
+	         report "COut should be 0 at 5ns"
+	         severity WARNING;
+	      assert (S = '0')
+	         report "S should be 0 at 5ns"
+	         severity WARNING;
+	      wait for 10ns;
+	      
+	      assert (COut = '0')
+	         report "COut should be 0 at 15ns"
+	         severity WARNING;
+	      assert (S = '1')
+	         report "S should be 1 at 5ns"
+	         severity WARNING;
+	      wait for 10ns;
+	      
+	      assert (COut = '0')
+	         report "COut should be 0 at 25ns"
+	         severity WARNING;
+	      assert (S = '1')
+	         report "S should be 1 at 25ns"
+	         severity WARNING;
+	      wait for 10ns;
+	      
+	      assert (COut = '1')
+	         report "COut should be 1 at 35ns"
+	         severity WARNING;
+	      assert (S = '0')
+	         report "S should be 0 at 35ns"
+	         severity WARNING;
+	      wait for 10ns;
+	      
+	      assert (COut = '0')
+	         report "COut should be 0 at 45ns"
+	         severity WARNING;
+	      assert (S = '1')
+	         report "S should be 1 at 45ns"
+	         severity WARNING;
+	      wait for 10ns;
+	      
+	      assert (COut = '1')
+	         report "COut should be 1 at 55ns"
+	         severity WARNING;
+	      assert (S = '0')
+	         report "S should be 0 at 55ns"
+	         severity WARNING;
+	      wait for 10ns;
+	      
+	      assert (COut = '1')
+	         report "COut should be 1 at 65ns"
+	         severity WARNING;
+	      assert (S = '0')
+	         report "S should be 0 at 65ns"
+	         severity WARNING;
+	      wait for 10ns;
+	      
+	      assert (COut = '1')
+	         report "COut should be 1 at 75ns"
+	         severity WARNING;
+	      assert (S = '1')
+	         report "S should be 1 at 75ns"
+	         severity WARNING;
+	      
+	      wait;  --Suspend
+	end process Assertions;
 end Bench;
