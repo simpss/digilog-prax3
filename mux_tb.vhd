d918b48353831f24ff0f03ced0fcc2629caf4a68
Siim Talvik <siim@talvik.eu>
2015-10-20 01:10:11 +0300

    initial assertions created for testbench

--- mux_tb.vhd
+++ mux_tb.vhd
@@ -28,7 +28,7 @@ begin
 
 --Component instantiation of MUX
 Mux_comp: MUX port map (X, Y, CIn, COut, S);
- 
+
 --Stimulus process
 Stimulus: process
    begin
@@ -37,7 +37,28 @@ Stimulus: process
       CIn <= '0' after 0ns, '1' after 10ns, '0' after 20ns, '1' after 30ns, '0' after 40ns, 
              '1' after 50ns, '0' after 60ns, '1' after 70ns, 'U' after 80ns;
       
+   wait;  --Suspend
+end process Stimulus;
+
+Assertions: process
+   begin
+      assert (COut = '0')
+         report "COut should be 0 at 5ns"
+         severity WARNING;
+      wait for 5ns;
+      assert (COut = '0')
+         report "COut should be 0 at 15ns"
+         severity WARNING;
+      wait for 10ns;
+      assert (COut = '0')
+         report "COut should be 0 at 25ns"
+         severity WARNING;
+      wait for 10ns;
+      assert (COut = '1')
+         report "COut should be 1 at 35ns"
+         severity WARNING;
+      
       wait;  --Suspend
-   end process Stimulus;
+end process Assertions;
 
 end Bench;
